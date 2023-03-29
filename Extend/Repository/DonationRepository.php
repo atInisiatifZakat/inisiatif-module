422ca5663f6292e1e205dc41edac519dad0f6a80
Nuradiyana <me@nooradiana.com>
2023-03-30 06:50:04 +0700

    Fixing dashboard by branch

--- Extend/Repository/DonationRepository.php
+++ Extend/Repository/DonationRepository.php
@@ -11,9 +11,11 @@ use Illuminate\Support\Collection;
 use Spatie\QueryBuilder\QueryBuilder;
 use Spatie\QueryBuilder\AllowedFilter;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Query\JoinClause;
 use Ziswapp\Domain\Foundation\Model\User;
 use Ziswapp\Domain\Transaction\Repository;
 use Ziswapp\Domain\Foundation\Model\Branch;
+use Ziswapp\Domain\Foundation\Enum\BranchType;
 use Ziswapp\Domain\Transaction\Model\Donation;
 use Ziswapp\QueryBuilder\Filter\DateTimeFilter;
 use Ziswapp\Domain\Transaction\Enum\DonationStatus;
@@ -29,16 +31,18 @@ final class DonationRepository implements Repository\Contract\DonationRepository
      */
     public function fetchAmountGroupByBranch(Branch $branch, DateTimeInterface $start, DateTimeInterface $end): Collection
     {
-        $branchIds = $branch->getSelfAndDescendants();
+        if ($branch->isHeadOffice()) {
+            return $this->fetchAmountGroupByBranchKc($start, $end);
+        }
 
         return Donation::query()
             ->join('branches', 'branches.id', '=', 'donations.branch_id')
             ->whereStatus(DonationStatus::verified)
-            ->whereBranches($branchIds)
+            ->whereBranches($branch->getSelfAndDescendants())
             ->whereBetween('transaction_at', [$start, $end])
             ->selectRaw('branches.name as branch, sum(amount) as aggregate')
             ->groupBy('branches.name')
-            ->orderBy('branches.name')
+            ->orderByRaw('sum(amount) DESC')
             ->get();
     }
 
@@ -54,7 +58,7 @@ final class DonationRepository implements Repository\Contract\DonationRepository
             ->whereBranches($branchIds)
             ->whereStatus(DonationStatus::verified)
             ->whereBetween('transaction_at', [$start, $end])
-            ->when($userIds, fn(DonationQueryBuilder $builder) => $builder->whereUsers($userIds))
+            ->when($userIds, fn (DonationQueryBuilder $builder) => $builder->whereUsers($userIds))
             ->sum('amount');
     }
 
@@ -70,7 +74,7 @@ final class DonationRepository implements Repository\Contract\DonationRepository
             ->whereBranches($branchIds)
             ->whereBetween('transaction_at', [$start, $end])
             ->whereStatusIn([DonationStatus::new, DonationStatus::paid])
-            ->when($user, fn(DonationQueryBuilder $builder) => $builder->whereUsers($userIds))
+            ->when($user, fn (DonationQueryBuilder $builder) => $builder->whereUsers($userIds))
             ->sum('amount');
     }
 
@@ -102,7 +106,7 @@ final class DonationRepository implements Repository\Contract\DonationRepository
         $builder = Donation::query()
             ->withSearch($keyword)
             ->where(
-                fn(Builder $builder) => $builder
+                fn (Builder $builder) => $builder
                     ->where('is_inisiatif_verified', false)
                     ->orWhereNull('is_inisiatif_verified')
             )
@@ -130,9 +134,9 @@ final class DonationRepository implements Repository\Contract\DonationRepository
     public function makeQueryBuilder(Builder|Relation|string $subject, ?Request $request = null): QueryBuilder
     {
         return QueryBuilder::for($subject, $request)->with([
-            'user' => fn(Relation $relation): Relation => $relation->select(['id', 'name']),
-            'branch' => fn(Relation $relation): Relation => $relation->select(['id', 'name']),
-            'donor' => fn(Relation $relation): Relation => $relation->select(['uuid', 'name']),
+            'user' => fn (Relation $relation): Relation => $relation->select(['id', 'name']),
+            'branch' => fn (Relation $relation): Relation => $relation->select(['id', 'name']),
+            'donor' => fn (Relation $relation): Relation => $relation->select(['uuid', 'name']),
         ])->allowedFilters([
             AllowedFilter::exact('type', 'donations.type'),
             AllowedFilter::exact('status', 'donations.status'),
@@ -151,6 +155,39 @@ final class DonationRepository implements Repository\Contract\DonationRepository
 
     public function findUsingReference(string $refId): ?Donation
     {
-        return Donation::query()->where('edonation_confirmation_id', $refId)->firstOr(static fn() => null);
+        return Donation::query()->where('edonation_confirmation_id', $refId)->firstOr(static fn () => null);
+    }
+
+    private function fetchAmountGroupByBranchKc(DateTimeInterface $start, DateTimeInterface $end, bool $withZero = false): Collection
+    {
+        $donationKcQuery = Donation::query()
+            ->selectRaw('branches.id as branch_id, sum(amount) as aggregate')
+            ->leftJoin('branches', 'branches.id', '=', 'donations.branch_id')
+            ->where('branches.type', BranchType::KC->value)
+            ->whereStatus(DonationStatus::verified)
+            ->whereBetween('transaction_at', [$start, $end])
+            ->groupBy('branches.id')
+            ->orderBy('branches.id');
+
+        $donationKcpQuery = Donation::query()
+            ->selectRaw('branches.parent_id as branch_id, sum(amount) as aggregate')
+            ->leftJoin('branches', 'branches.id', '=', 'donations.branch_id')
+            ->where('branches.type', BranchType::KCP->value)
+            ->whereStatus(DonationStatus::verified)
+            ->whereBetween('transaction_at', [$start, $end])
+            ->groupBy('branches.parent_id')
+            ->orderBy('branches.parent_id');
+
+        return Branch::query()
+            ->selectRaw('branches.id, branches.name as branch, sum(coalesce(kc.aggregate, 0) + coalesce(kcp.aggregate, 0)) as aggregate')
+            ->leftJoinSub($donationKcQuery, 'kc', function (JoinClause $join): void {
+                $join->on('branches.id', '=', 'kc.branch_id');
+            })->leftJoinSub($donationKcpQuery, 'kcp', function (JoinClause $join): void {
+                $join->on('branches.id', '=', 'kcp.branch_id');
+            })
+            ->when(! $withZero, fn (Builder $builder) => $builder->havingRaw('sum(coalesce(kc.aggregate, 0) + coalesce(kcp.aggregate, 0)) > 0'))
+            ->groupBy('branches.id', 'branches.name')
+            ->orderByRaw('sum(coalesce(kc.aggregate, 0) + coalesce(kcp.aggregate, 0)) DESC')
+            ->get();
     }
 }
